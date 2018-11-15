5a1b7f9d2e3b1d397cdcc4799aa7c7f2f8bbff2f
Erick <ericon99sdb@gmail.com>
2018-11-15 07:37:12 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
+const BodyParser = require('body-parser');
 
 //Conexion a la base de datos
 const mongoose = require('mongoose');
@@ -12,40 +11,27 @@ mongoose.Promise = global.Promise;
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({extend:false}));
-app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', '*');
-
-    if(req.method === 'OPTIONS'){
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-        return res.status(200).json({});
-    }
-    next();
-});
+app.use(BodyParser.urlencoded({extended:false}));
+app.use(BodyParser.json());
 
 const pizzaRoutes = require('./routes/pizza');
-const ordersRoutes = require('./routes/orders');
 
 app.use('/pizza', pizzaRoutes);
-app.use('/orders', ordersRoutes);
 
 app.use((req, res, next) => {
     const error = new Error('Not found');
-    error.status(404);
+    error.status = 404;
     next(error);
-})
+});
 
 app.use((error, req, res, next) => {
     res.status(error.status || 500);
     res.json({
-        error:{
+        error: {
             message: error.message
         }
     });
-})
+});
 
 module.exports = app;
